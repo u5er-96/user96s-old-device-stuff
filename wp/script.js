33c56d7d030680ef622ea72e9d2b945fa40af75c
U53R-96 <viales7587@gmail.com>
2025-04-20 18:23:25 -0700

    edited more files

--- wp/script.js
+++ wp/script.js
@@ -1,15 +1,7 @@
-//Brandon Blanchard
-//1280x720 screen scaled to half size
-//http://i-cdn.phonearena.com/images/phones/37940-xlarge/Nokia-Lumia-920.jpg
-
-$(document).ready(function() {
-     $('.wp-large div, .wp-med div, .wp-small div' ).each(function(tile) {
-        var thisPos = $(this).offset(),
-        screen = $('#lumia-screen').offset();
-        
-      console.log('background-position-y', '-' + (thisPos.top - screen.top) + 'px');
-      
-        $(this).css('background-position-y', '-' + (thisPos.top - screen.top) + 'px');
-        $(this).css('background-position-x', '-' + (thisPos.left - screen.left) + 'px');
-    });
+$('#animator').on('mouseover',function(){
+   $('.tile').addClass('leftRotate').removeClass('rightRotate'); 
+});
+    
+$('#animator').on('mouseout',function(){
+   $('.tile').addClass('rightRotate').removeClass('leftRotate'); 
 });
