c64391bbccea00e3e0d650ca9f9e2612e8e33ff2
U53R-96 <viales7587@gmail.com>
2025-04-20 18:49:03 -0700

    new wp page

--- wp/script.js
+++
@@ -1,7 +0,0 @@
-$('#animator').on('mouseover',function(){
-   $('.tile').addClass('leftRotate').removeClass('rightRotate'); 
-});
-    
-$('#animator').on('mouseout',function(){
-   $('.tile').addClass('rightRotate').removeClass('leftRotate'); 
-});
